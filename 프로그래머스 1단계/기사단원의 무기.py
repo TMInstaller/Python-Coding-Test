53d58af778e384a9d747861beefe5430d55c3409
TMInstaller <ajvldmlqjqclr@naver.com>
2023-02-10 22:04:29 +0900

    풀이 추가

---
+++ 프로그래머스 1단계/기사단원의 무기.py
@@ -0,0 +1,27 @@
+# 풀이 추가
+def solution(number, limit, power):
+    answer = 0
+    knight = []
+    divisor_knight = []
+
+    # 기사번호 담기
+    for i in range(number):
+        knight.append(i+1)
+
+    # 각 기사번호 별 약수 담기
+    for k in knight:
+        tmp = []
+        for i in range(1, int(k**(1/2)) + 1):
+            if k % i == 0:
+                tmp.append(i)
+                if ((i**2) != k):
+                    tmp.append(k // i)
+        divisor_knight.append(len(tmp))
+
+    for d in divisor_knight:
+        if d <= limit:
+            answer += d
+        else:
+            answer += power
+
+    return answer
