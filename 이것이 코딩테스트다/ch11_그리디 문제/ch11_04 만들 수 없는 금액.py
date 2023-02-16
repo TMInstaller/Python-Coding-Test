af8fd70538b09e614a618e2735b815461d3aae67
TMInstaller <ajvldmlqjqclr@naver.com>
2023-02-16 21:14:48 +0900

    1회차

---
+++ 이것이 코딩테스트다/ch11_그리디 문제/ch11_04 만들 수 없는 금액.py
@@ -0,0 +1,11 @@
+# 1회 - 풀이 참고
+n = int(input())
+coins = list(map(int, input().split()))
+coins.sort()
+target = 1
+for i in coins:
+    if target < i:
+        break
+    target += i
+
+print(target)
