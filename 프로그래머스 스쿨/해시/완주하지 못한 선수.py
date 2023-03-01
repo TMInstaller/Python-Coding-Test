5ce593acb953e4be7c29bd9b26a2b8617ad0b6f4
TMInstaller <ajvldmlqjqclr@naver.com>
2023-03-01 11:03:48 +0900

    해시 문제

---
+++ 프로그래머스 스쿨/해시/완주하지 못한 선수.py
@@ -0,0 +1,11 @@
+def solution(participant, completion):
+    answer = ''
+    dic = {}
+    tmp = 0
+    for p in participant:
+        dic[hash(p)] = p
+        tmp += hash(p)
+
+    for c in completion:
+        tmp -= hash(c)
+    return dic[tmp]
